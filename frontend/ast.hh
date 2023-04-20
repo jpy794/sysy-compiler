d451b7df05463cf72163e980af2cfe02182148c8
jpy794 <jpy794@hotmail.com>
2023-04-20 15:13:24 +0800

    refactor: split ast nodes into 3 classes

--- frontend/ast.hh
+++ frontend/ast.hh
@@ -2,6 +2,7 @@
 
 #include <any>
 #include <memory>
+#include <optional>
 #include <stdexcept>
 #include <string>
 #include <utility>
@@ -14,63 +15,48 @@ template <typename T> using Ptr = std::unique_ptr<T>;
 template <typename T> using PtrList = std::vector<Ptr<T>>;
 
 enum class BaseType { VOID, FLOAT, INT };
-struct SysYType {
-    BaseType base;
-    /* dim[0] = 0 means that this is a pointer,
-       which is only allowed in function parameter */
-    std::vector<size_t> dims;
-    bool is_ptr() const { return dims.size() > 0 && dims[0] == 0; }
-    static SysYType get_type(BaseType base, std::vector<size_t> &&dims) {
-        return {base, dims};
-    }
-};
 
-/* NOP: interpret the cond as it is */
-enum class CondOp { LT, GT, LE, GE, EQ, NE, /* unary op */ NOT, NOP };
-enum class ExpOp { ADD, SUB, MUL, DIV, MOD, /* unary op */ PLUS, MINUS };
+enum class BinOp { ADD, SUB, MUL, DIV, MOD, LT, GT, LE, GE, EQ, NE };
+enum class UnaryOp { PlUS, MINUS, /* only apply to cond expr */ NOT };
 
 class ASTVisitor;
 struct ASTNode {
     virtual ~ASTNode() = default;
     virtual std::any accept(ASTVisitor &visitor) const {
-        /* visit phantom node will trigger this exception
-           e.g. ASTNode GlobalDefNode StmtNode StmtOrVarDefNode CondOrExpNode
+        /* visiting a phantom node will trigger this exception
+           e.g. Expr Stmt Global
            in case any pure virtual method call */
         throw std::runtime_error{"unexpected phantom ASTNode visit."};
     };
 };
 
 /* AST node structure */
-struct RootNode;
+struct Root;
 
-/* global def */
-struct GlobalDefNode;
-struct FuncDefNode;
+/* global */
+struct Global : ASTNode {};
+struct FunDefGlobal;
+struct VarDefGlobal;
 
-/* stmt or def */
-struct StmtOrVarDefNode;
-struct StmtNode;
-struct BlockNode;
-struct IfNode;
-struct BreakNode;
-struct WhileNode;
-struct ContinueNode;
-struct ReturnNode;
-struct AssignNode;
-struct VarDefNode; // also global def
+/* stmt: return ir or bb */
+struct Stmt : ASTNode {};
+struct BlockStmt;
+struct IfStmt;
+struct BreakStmt;
+struct WhileStmt;
+struct ContinueStmt;
+struct ReturnStmt;
+struct AssignStmt;
+struct VarDefStmt;
+struct ExprStmt;
 
-/* cond or exp */
-struct CondOrExpNode;
-struct CondNode;
-struct ExpNode; // also stmt, init
-struct ArithExpNode;
-struct CallNode;
-struct LiteralNode;
-struct LValNode;
-
-/* init exps */
-struct InitNode;
-struct ArrayInitNode;
+/* expr: return a variable and ir that generates it */
+struct Expr : ASTNode {};
+struct UnaryExpr;
+struct BinaryExpr;
+struct CallExpr;
+struct LiteralExpr;
+struct LValExpr;
 
 /* visitor pattern allows multiple nodes of the same super class
    to be visited through super class pointer (by the virtual accept).
@@ -87,137 +73,84 @@ class ASTVisitor {
     /* visit for super class pointer */
     std::any visit(const ASTNode &node) { return node.accept(*this); }
     /* do NOT save pointer to AST tree node in visitor */
-    virtual std::any visit(const RootNode &node) = 0;
-    virtual std::any visit(const FuncDefNode &node) = 0;
-    virtual std::any visit(const BlockNode &node) = 0;
-    virtual std::any visit(const CondNode &node) = 0;
-    virtual std::any visit(const IfNode &node) = 0;
-    virtual std::any visit(const WhileNode &node) = 0;
-    virtual std::any visit(const BreakNode &node) = 0;
-    virtual std::any visit(const ContinueNode &node) = 0;
-    virtual std::any visit(const ReturnNode &node) = 0;
-    virtual std::any visit(const AssignNode &node) = 0;
-    virtual std::any visit(const VarDefNode &node) = 0;
-    virtual std::any visit(const CallNode &node) = 0;
-    virtual std::any visit(const LiteralNode &node) = 0;
-    virtual std::any visit(const LValNode &node) = 0;
-    virtual std::any visit(const ArithExpNode &node) = 0;
-    virtual std::any visit(const InitNode &node) = 0;
-    virtual std::any visit(const ArrayInitNode &node) = 0;
+    virtual std::any visit(const Root &node) = 0;
+    /* global */
+    virtual std::any visit(const FunDefGlobal &node) = 0;
+    virtual std::any visit(const VarDefGlobal &node) = 0;
+    /* stmt */
+    virtual std::any visit(const BlockStmt &node) = 0;
+    virtual std::any visit(const IfStmt &node) = 0;
+    virtual std::any visit(const WhileStmt &node) = 0;
+    virtual std::any visit(const BreakStmt &node) = 0;
+    virtual std::any visit(const ContinueStmt &node) = 0;
+    virtual std::any visit(const ReturnStmt &node) = 0;
+    virtual std::any visit(const AssignStmt &node) = 0;
+    virtual std::any visit(const VarDefStmt &node) = 0;
+    virtual std::any visit(const ExprStmt &node) = 0;
+    /* expr */
+    virtual std::any visit(const CallExpr &node) = 0;
+    virtual std::any visit(const LiteralExpr &node) = 0;
+    virtual std::any visit(const LValExpr &node) = 0;
+    virtual std::any visit(const BinaryExpr &node) = 0;
+    virtual std::any visit(const UnaryExpr &node) = 0;
 };
 
-struct RootNode : ASTNode {
-    PtrList<GlobalDefNode> global_defs;
+struct Root : ASTNode {
+    PtrList<Global> globals;
     std::any accept(ASTVisitor &visitor) const override {
         return visitor.visit(*this);
     }
 };
 
-struct GlobalDefNode : ASTNode {};
-
-struct FuncDefNode : GlobalDefNode {
+struct FunDefGlobal : Global {
+    struct Param {
+        BaseType type;
+        std::string name;
+        /* evaluated when building AST */
+        std::vector<size_t> dims;
+    };
     /* void, int or float */
     BaseType ret_type;
-    std::string name;
-    std::vector<SysYType> args;
-    Ptr<BlockNode> body;
+    std::string fun_name;
+    std::vector<Param> params;
+    Ptr<BlockStmt> body;
     std::any accept(ASTVisitor &visitor) const override {
         return visitor.visit(*this);
     }
 };
 
-struct StmtOrVarDefNode : ASTNode {};
-struct StmtNode : StmtOrVarDefNode {};
-
-struct BlockNode : StmtNode {
-    PtrList<StmtOrVarDefNode> stmt_defs;
+struct VarDefGlobal : Global {
+    Ptr<VarDefStmt> vardef_stmt;
     std::any accept(ASTVisitor &visitor) const override {
         return visitor.visit(*this);
     }
 };
 
-struct CondOrExpNode : ASTNode {};
-
-struct CondNode : CondOrExpNode {
-    CondOp op;
-    /* for unary op, rhs is the only child
-       lhs, rhs can be exp or cond node */
-    Ptr<CondOrExpNode> lhs, rhs;
+struct UnaryExpr : Expr {
+    UnaryOp op;
+    Ptr<Expr> rhs;
     std::any accept(ASTVisitor &visitor) const override {
         return visitor.visit(*this);
     }
 };
 
-struct InitNode : ASTNode {
-    PtrList<InitNode> init_exps;
+struct BinaryExpr : Expr {
+    BinOp op;
+    Ptr<Expr> lhs, rhs;
     std::any accept(ASTVisitor &visitor) const override {
         return visitor.visit(*this);
     }
 };
 
-struct ExpNode : StmtNode, CondOrExpNode, InitNode {};
-
-struct ArithExpNode : ExpNode {
-    ExpOp op;
-    /* for unary op, rhs is the only child */
-    Ptr<ExpNode> lhs, rhs;
-    std::any accept(ASTVisitor &visitor) const override {
-        return visitor.visit(*this);
-    }
-};
-
-struct IfNode : StmtNode {
-    Ptr<CondNode> cond;
-    Ptr<StmtNode> then_body, else_body;
-    std::any accept(ASTVisitor &visitor) const override {
-        return visitor.visit(*this);
-    }
-};
-
-struct WhileNode : StmtNode {
-    Ptr<CondNode> cond;
-    Ptr<StmtNode> body;
-    std::any accept(ASTVisitor &visitor) const override {
-        return visitor.visit(*this);
-    }
-};
-
-struct BreakNode : StmtNode {
-    std::any accept(ASTVisitor &visitor) const override {
-        return visitor.visit(*this);
-    }
-};
-
-struct ContinueNode : StmtNode {
-    std::any accept(ASTVisitor &visitor) const override {
-        return visitor.visit(*this);
-    }
-};
-
-struct ReturnNode : StmtNode {
-    Ptr<ExpNode> ret_val;
-    std::any accept(ASTVisitor &visitor) const override {
-        return visitor.visit(*this);
-    }
-};
-
-struct AssignNode : StmtNode {
-    Ptr<LValNode> lval;
-    Ptr<ExpNode> exp;
-    std::any accept(ASTVisitor &visitor) const override {
-        return visitor.visit(*this);
-    }
-};
-
-struct LValNode : ExpNode {
+struct LValExpr : Expr {
     std::string var_name;
-    std::vector<size_t> dims;
+    PtrList<Expr> idxs;
     std::any accept(ASTVisitor &visitor) const override {
         return visitor.visit(*this);
     }
 };
 
-struct LiteralNode : ExpNode {
+struct LiteralExpr : Expr {
     BaseType type;
     std::variant<float, int> val;
     std::any accept(ASTVisitor &visitor) const override {
@@ -225,26 +158,84 @@ struct LiteralNode : ExpNode {
     }
 };
 
-struct CallNode : ExpNode {
-    std::string func_name;
-    PtrList<ExpNode> exps;
+struct CallExpr : Expr {
+    std::string fun_name;
+    PtrList<Expr> args;
     std::any accept(ASTVisitor &visitor) const override {
         return visitor.visit(*this);
     }
 };
 
-struct VarDefNode : GlobalDefNode, StmtOrVarDefNode {
-    bool is_const;
+struct IfStmt : Stmt {
+    Ptr<Expr> cond;
+    Ptr<Stmt> then_body, else_body;
+    std::any accept(ASTVisitor &visitor) const override {
+        return visitor.visit(*this);
+    }
+};
+
+struct WhileStmt : Stmt {
+    Ptr<Expr> cond;
+    Ptr<Stmt> body;
+    std::any accept(ASTVisitor &visitor) const override {
+        return visitor.visit(*this);
+    }
+};
+
+struct BreakStmt : Stmt {
+    std::any accept(ASTVisitor &visitor) const override {
+        return visitor.visit(*this);
+    }
+};
+
+struct ContinueStmt : Stmt {
+    std::any accept(ASTVisitor &visitor) const override {
+        return visitor.visit(*this);
+    }
+};
+
+struct ReturnStmt : Stmt {
+    Ptr<Expr> ret_val;
+    std::any accept(ASTVisitor &visitor) const override {
+        return visitor.visit(*this);
+    }
+};
+
+struct AssignStmt : Stmt {
     std::string var_name;
-    SysYType type;
-    Ptr<InitNode> init_val;
+    PtrList<Expr> idxs;
+    Ptr<Expr> val;
     std::any accept(ASTVisitor &visitor) const override {
         return visitor.visit(*this);
     }
 };
 
-struct ArrayInitNode : InitNode {
-    PtrList<InitNode> init_vals;
+struct VarDefStmt : Stmt {
+    bool is_const;
+    BaseType type;
+    std::string var_name;
+    /* all dims should be non-zero
+       evaluated when building the AST */
+    std::vector<size_t> dims;
+    /* TODO: evaluate init_vals for global consts
+       instead of leaving it to optimization pass later
+       nullopt means undefined init_val for corresponding element */
+    std::vector<std::optional<Ptr<Expr>>> init_vals;
+    std::any accept(ASTVisitor &visitor) const override {
+        return visitor.visit(*this);
+    }
+};
+
+struct BlockStmt : Stmt {
+    PtrList<Stmt> stmts;
+    std::any accept(ASTVisitor &visitor) const override {
+        return visitor.visit(*this);
+    }
+};
+
+struct ExprStmt : Stmt {
+    /* nullopt means it is an empty stmt, aka a semicolon */
+    std::optional<Ptr<Expr>> expr;
     std::any accept(ASTVisitor &visitor) const override {
         return visitor.visit(*this);
     }
@@ -260,6 +251,6 @@ class AST {
     }
 
   private:
-    Ptr<RootNode> root;
+    Ptr<Root> root;
 };
 } // namespace ast
