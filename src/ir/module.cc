efa1af8f54b362b5872b5af9ece7535648da212e
Wisgy <2306427376@qq.com>
2023-05-01 16:14:41 +0000

    feat: add cached func

--- src/ir/module.cc
+++ src/ir/module.cc
@@ -1,9 +1,13 @@
 #include "module.hh"
+#include "constant.hh"
+#include "type.hh"
+#include <memory>
 
 using namespace ir;
 
 Module::Module(std::string &name) : _name(name) {
-    _int_ty = std::make_unique<IntType>(this, 32);
+    _int1_ty = std::make_unique<IntType>(this, 1);
+    _int32_ty = std::make_unique<IntType>(this, 32);
     _float_ty = std::make_unique<FloatType>(this);
     _void_ty = std::make_unique<VoidType>(this);
     _label_ty = std::make_unique<LabelType>(this);
@@ -29,4 +33,31 @@ Type *Module::get_pointer_type(Type *elementTp) {
 
 Type *Module::get_function_type(Type *ret, const std::vector<Type *> &params) {
     return new FuncType(this, ret, params);
+}
+ConstantInt *Module::be_cached(bool val) {
+    if (this->_cached_bool.find(val) != _cached_bool.end())
+        return _cached_bool[val].get();
+    else {
+        return (_cached_bool[val] = std::unique_ptr<ConstantInt>(
+                    new ConstantInt(this->get_int1_type(), val)))
+            .get();
+    }
+}
+ConstantInt *Module::be_cached(int val) {
+    if (this->_cached_int.find(val) != _cached_int.end())
+        return _cached_int[val].get();
+    else {
+        return (_cached_int[val] = std::unique_ptr<ConstantInt>(
+                    new ConstantInt(this->get_int32_type(), val)))
+            .get();
+    }
+}
+ConstantFloat *Module::be_cached(float val) {
+    if (this->_cached_float.find(val) != _cached_float.end())
+        return _cached_float[val].get();
+    else {
+        return (_cached_float[val] = std::unique_ptr<ConstantFloat>(
+                    new ConstantFloat(this->get_float_type(), val)))
+            .get();
+    }
 }
