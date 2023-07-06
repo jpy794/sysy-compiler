fdb7652fbd74ec5bb74c26b9ef4b7c0c0f5f4e22
lxq <877250099@qq.com>
2023-07-06 11:21:07 +0800

    chore: add not_implemented_error in err.hh

--- src/utils/err.hh
+++ src/utils/err.hh
@@ -8,3 +8,9 @@ class unreachable_error : public std::logic_error {
     unreachable_error(const std::string &what_arg) : logic_error{what_arg} {}
     const char *what() const noexcept final { return logic_error::what(); }
 };
+
+class not_implemented_error : public std::logic_error {
+  public:
+    not_implemented_error()
+        : std::logic_error("Function not yet implemented"){};
+};
