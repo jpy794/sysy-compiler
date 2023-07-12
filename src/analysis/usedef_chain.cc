b32157469ca38e373ec4ef3a1c1a1fe1caf5924e
Wisgy <2306427376@qq.com>
2023-07-12 02:23:46 +0000

    fix: usedef init its Result

--- src/analysis/usedef_chain.cc
+++ src/analysis/usedef_chain.cc
@@ -8,6 +8,8 @@ void UseDefChain::run(pass::PassManager *mgr) {
         for (auto &bb : func.bbs()) {
             for (auto &inst : bb.insts()) {
                 auto user = dynamic_cast<ir::User *>(&inst);
+                if (_result.users.find(&inst) == _result.users.end()) // init
+                    _result.users[&inst] = {};
                 for (unsigned idx = 0; idx < inst.operands().size(); idx++) {
                     _result.users[inst.operands()[idx]].push_back({user, idx});
                 }
