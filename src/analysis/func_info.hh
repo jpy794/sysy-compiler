59d1787be29f57a92a1a561480dee43b607c9686
Wisgy <2306427376@qq.com>
2023-07-12 02:24:39 +0000

    feat: func_info can get callers & purefunc

---
+++ src/analysis/func_info.hh
@@ -0,0 +1,49 @@
+#pragma once
+#include "function.hh"
+#include "instruction.hh"
+#include "pass.hh"
+#include "usedef_chain.hh"
+#include "utils.hh"
+#include <deque>
+#include <set>
+
+namespace pass {
+class FuncInfo final : public pass::AnalysisPass {
+  public:
+    explicit FuncInfo() {}
+    ~FuncInfo() = default;
+
+    struct ResultType {
+        std::set<ir::Function *> pure_functions{};
+        std::map<ir::Function *, std::set<ir::Function *>> callers{};
+
+        bool is_pure_function(ir::Function *inst) const {
+            return contains(pure_functions, inst);
+        }
+    };
+
+    virtual void get_analysis_usage(pass::AnalysisUsage &AU) const override {
+        using KillType = pass::AnalysisUsage::KillType;
+        AU.set_kill_type(KillType::None);
+    }
+
+    virtual std::any get_result() const override { return &_result; }
+
+    virtual void run(pass::PassManager *mgr) override;
+
+    virtual void clear() override {
+        _result.pure_functions.clear();
+        _result.callers.clear();
+        callee_pure.clear();
+    }
+
+  private:
+    bool maybe_pure(ir::Function *func);
+    bool is_side_effect_inst(ir::Instruction *inst);
+    ir::Value *get_origin_addr(ir::Value *addr);
+
+    ResultType _result;
+
+    std::deque<ir::Function *> callee_pure;
+};
+} // namespace pass
