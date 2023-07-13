0b8f7ad1b12750a2d676da3c26f0443d9db2a12f
lxq <877250099@qq.com>
2023-07-13 14:44:27 +0800

    chore: add a test to ouput to stdout

--- test/codegen/simple_test.cc
+++ test/codegen/simple_test.cc
@@ -14,21 +14,28 @@ using namespace std;
 using namespace filesystem;
 
 int main(int argc, char **argv) {
-    if (argc < 7) {
+    if (argc != 7) {
         throw std::runtime_error{"wrong arguement"};
     }
 
     auto test_case = string{argv[1]};
 
     // tmp folder for binary and output
-    auto output_path = path{argv[2]};
-    create_directories(output_path);
+    auto tmp_path = path{argv[2]};
+    create_directories(tmp_path);
+    /* auto tmp = [&](const string &suffix) {
+     *     return tmp_path / (test_case + suffix);
+     * };  */
 
     // paths that store the testcases
     auto sysy_path = path{argv[3]};
+    auto in_path = path{argv[4]};
+    auto out_path = path{argv[5]};
     auto test = [&](const path &base, const string &suffix) {
         return base / (test_case + suffix);
     };
+
+    auto lib_file = path{argv[6]};
 
     if (not is_regular_file(test(sysy_path, ".sy"))) {
         throw runtime_error{"sysy source not exist"};
@@ -36,6 +43,7 @@ int main(int argc, char **argv) {
 
     ast::AST ast{ast::RawAST{test(sysy_path, ".sy")}};
     IRBuilder builder{ast};
+
     auto module = builder.release_module();
     pass::PassManager pm(std::move(module));
     pm.add_pass<pass::RmUnreachBB>();
@@ -45,10 +53,5 @@ int main(int argc, char **argv) {
     pm.run();
 
     codegen::CodeGen codegen{pm.release_module()};
-
-    ofstream asm_output{output_path / (test_case + ".s")};
-    asm_output << codegen;
-    asm_output.close();
-
-    return 0;
+    cout << codegen;
 }
