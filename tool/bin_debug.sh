529770968c5fbdf8af523691b1c568b6055920dd
jpy794 <jpy794@hotmail.com>
2023-07-27 12:51:39 +0800

    chore: allow specifying input file for bin_debug

--- tool/bin_debug.sh
+++ tool/bin_debug.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# usage: ./tool/bin_debug.sh path/to/bin
+# usage: ./tool/bin_debug.sh path/to/bin path/to/input
 
 bin=$1
+stdin=$2
 port=1234
 
-qemu-riscv64-static -L /usr/riscv64-linux-gnu -g $port $bin &
+if [ -z $stdin ]; then
+    qemu-riscv64-static -L /usr/riscv64-linux-gnu -g $port $bin &
+else
+    qemu-riscv64-static -L /usr/riscv64-linux-gnu -g $port $bin < $stdin &
+fi
+
 riscv64-linux-gnu-gdb -q \
     -ex 'set sysroot /usr/riscv64-linux-gnu' \
     -ex "file $bin" \
