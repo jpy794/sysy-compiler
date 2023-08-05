d86a5fb2f8576f6f7108618c771ef65d0ca4b3ac
Wisgy <sunc_y_@mail.ustc.edu.cn>
2023-08-05 16:38:28 +0800

    feat: primary inline pass

---
+++ src/transform/inline.cc
@@ -0,0 +1,155 @@
+#include "inline.hh"
+#include "depth_order.hh"
+#include "function.hh"
+#include "instruction.hh"
+#include "type.hh"
+#include "usedef_chain.hh"
+#include "utils.hh"
+#include <cassert>
+#include <deque>
+#include <iostream>
+
+using namespace std;
+using namespace pass;
+using namespace ir;
+
+bool Inline::is_inline(Function *func) {
+    if (func->is_external)
+        return false;
+    return true;
+}
+
+void Inline::run(PassManager *mgr) {
+    auto m = mgr->get_module();
+    _use_def_chain = &mgr->get_result<UseDefChain>();
+    for (auto &f_r : m->functions()) {
+        if (f_r.get_name() == "@main") {
+            for (auto &bb_r : f_r.bbs()) {
+                for (auto iter = bb_r.insts().begin();
+                     iter != bb_r.insts().end(); ++iter) {
+                    if (is_a<CallInst>(&*iter) &&
+                        is_inline(as_a<Function>(iter->get_operand(0)))) {
+                        clee2cler.clear();
+                        inline_bb.clear();
+                        inline_func(iter);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void Inline::inline_func(ilist<ir::Instruction>::iterator callee) {
+    auto caller_func = callee->get_parent()->get_func();
+    auto callee_func = as_a<Function>(callee->get_operand(0));
+    // cast args to parameters
+    auto &args = callee_func->get_args();
+    for (unsigned i = 1; i < callee->operands().size(); i++) {
+        clee2cler[args[i - 1]] = callee->get_operand(i);
+    }
+    clone(callee_func, caller_func);
+    replace(callee);
+    trivial(callee);
+    // for (auto bb : inline_bb) {
+    //     cout << bb->print() << endl;
+    // }
+}
+
+void Inline::clone(Function *callee, Function *caller) {
+    auto entry_bb = caller->create_bb();
+    clee2cler[callee->get_entry_bb()] = entry_bb;
+    deque<BasicBlock *> bb_work_list;
+    bb_work_list.push_back(callee->get_entry_bb());
+    inline_bb.push_back(entry_bb);
+    while (not bb_work_list.empty()) {
+        auto top = bb_work_list.front();
+        auto map_bb = as_a<BasicBlock>(clee2cler[top]);
+        bb_work_list.pop_front();
+        // step1 create successor bb to be inserted
+        for (auto suc_bb : top->get_suc_bbs()) {
+            if (clee2cler[suc_bb] == nullptr) {
+                auto bb = caller->create_bb();
+                clee2cler[suc_bb] = bb;
+                bb_work_list.push_back(suc_bb);
+                inline_bb.push_back(bb);
+            }
+            // maintain pre/suc relationship
+            auto bb = as_a<BasicBlock>(clee2cler[suc_bb]);
+            map_bb->suc_bbs().push_back(bb);
+            bb->pre_bbs().push_back(map_bb);
+        }
+        // step2 clone inst from callee function into caller function
+        for (auto &inst_r : top->insts()) {
+            auto inst = &inst_r;
+            auto map_inst =
+                map_bb->clone_inst_skeleton(map_bb->insts().end(), inst);
+            clee2cler[inst] = map_inst;
+        }
+    }
+}
+
+void Inline::replace(
+    ilist<ir::Instruction>::iterator call_iter) { // replace operands of inst
+                                                  // with map_val because the
+    // operands now belong to callee function
+    for (auto bb : inline_bb) {
+        for (auto &inst_r : bb->insts()) {
+            for (unsigned i = 0; i < inst_r.operands().size(); i++) {
+                auto oper = inst_r.get_operand(i);
+                if (is_a<Argument>(oper) || is_a<BasicBlock>(oper) ||
+                    is_a<Instruction>(oper)) {
+                    assert(clee2cler[oper]);
+                    inst_r.set_operand(i, clee2cler[oper]);
+                }
+                // for constant, global and function, it shouldn't be replaced
+            }
+        }
+    }
+    auto callee_func = as_a<Function>(call_iter->get_operand(0));
+    auto callee_exit_bb = callee_func->get_exit_bb();
+    auto map_exit_bb = as_a<BasicBlock>(clee2cler[callee_exit_bb]);
+    assert(is_a<RetInst>(&map_exit_bb->insts().back()));
+    if (is_a<VoidType>(callee_func->get_return_type()))
+        return;
+    // replace call result with return val
+    if (_use_def_chain->users.find(&*call_iter) != _use_def_chain->users.end())
+        _use_def_chain->replace_all_use_with(
+            &*call_iter, map_exit_bb->insts().back().get_operand(0));
+}
+
+void Inline::trivial(ilist<ir::Instruction>::iterator call_iter) {
+    // step1 erase return inst within map_exit_bb
+    auto callee = as_a<Function>(call_iter->get_operand(0));
+    auto map_exit_bb = as_a<BasicBlock>(clee2cler[callee->get_exit_bb()]);
+    map_exit_bb->insts().erase(&map_exit_bb->insts().back());
+    // step2 move insts after call_inst from parent_bb to map_exit_bb
+    auto parent_bb = call_iter->get_parent();
+    auto move_iter = call_iter;
+    // count which instructions need to be moved
+    deque<Instruction *> move_insts;
+    for (++move_iter; move_iter != parent_bb->insts().end(); ++move_iter) {
+        move_insts.push_back(&*move_iter);
+    }
+    for (auto inst : move_insts) {
+        map_exit_bb->move_inst(map_exit_bb->insts().end(), inst);
+    }
+    // modify phi's source according to current bb's relationship
+    for (auto br_tar_bb : map_exit_bb->suc_bbs()) {
+        for (auto &inst_r : br_tar_bb->insts()) {
+            if (is_a<PhiInst>(&inst_r)) {
+                for (unsigned i = 1; i < inst_r.operands().size(); i += 2) {
+                    if (inst_r.operands()[i] == parent_bb) {
+                        inst_r.operands()[i] = map_exit_bb;
+                    }
+                }
+            } else
+                break;
+        }
+    }
+    map_exit_bb->suc_bbs() = parent_bb->suc_bbs();
+    // step3 replace call_inst with a jump inst to map_entry_bb
+    auto map_entry_bb = as_a<BasicBlock>(clee2cler[callee->get_entry_bb()]);
+    parent_bb->insts().erase(&*call_iter);
+    parent_bb->create_inst<BrInst>(map_entry_bb);
+}
---
+++ src/transform/inline.hh
@@ -0,0 +1,40 @@
+#pragma once
+#include "const_propagate.hh"
+#include "depth_order.hh"
+#include "function.hh"
+#include "ilist.hh"
+#include "instruction.hh"
+#include "pass.hh"
+#include "remove_unreach_bb.hh"
+#include "usedef_chain.hh"
+#include "value.hh"
+#include <deque>
+#include <unordered_map>
+
+namespace pass {
+
+class Inline final : public pass::TransformPass {
+  public:
+    Inline() = default;
+    virtual void get_analysis_usage(pass::AnalysisUsage &AU) const override {
+        using KillType = pass::AnalysisUsage::KillType;
+        AU.set_kill_type(KillType::All);
+        AU.add_require<pass::UseDefChain>();
+        AU.add_require<DepthOrder>();
+        // AU.add_post<RmUnreachBB>();
+        // AU.add_post<ConstPro>();
+    }
+    virtual void run(pass::PassManager *mgr) override;
+
+  private:
+    bool is_inline(ir::Function *);
+    void inline_func(ilist<ir::Instruction>::iterator);
+    void clone(ir::Function *, ir::Function *);
+    void replace(ilist<ir::Instruction>::iterator);
+    void trivial(ilist<ir::Instruction>::iterator);
+    std::unordered_map<const ir::Value *, ir::Value *> clee2cler;
+    std::deque<ir::BasicBlock *> inline_bb;
+    const pass::UseDefChain::ResultType *_use_def_chain;
+};
+
+}; // namespace pass
