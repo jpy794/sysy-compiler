704c2faf9c6d61e39ee551e7be8755f34b1936b9
lxq <877250099@qq.com>
2023-08-14 08:57:47 +0800

    refactor: let pass::run return bool(if modified)

--- src/pass/pass.hh
+++ src/pass/pass.hh
@@ -33,7 +33,7 @@ class Pass {
     Pass &operator=(const Pass &) = delete;
 
     virtual ~Pass() {}
-    virtual void run(PassManager *mgr) = 0;
+    virtual bool run(PassManager *mgr) = 0;
     virtual void get_analysis_usage(AnalysisUsage &AU) const {}
     virtual bool always_invalid() const { return false; }
 };
@@ -106,6 +106,11 @@ class TransformPass : public Pass {
     virtual bool always_invalid() const override { return true; }
 };
 
+class IterativePass : public Pass {
+  public:
+    explicit IterativePass() = default;
+};
+
 class PassManager {
     class PassInfo {
         Ptr<Pass> ptr;
@@ -156,6 +161,7 @@ class PassManager {
     }
 
     void run(const PassOrder &o, bool post = true);
+    void run_iteratively(const PassOrder &order);
 
     void reset() {
         for (auto &[_, info] : _passes)
@@ -181,5 +187,7 @@ class PassManager {
                                    pass_name + ">() has been called"};
         }
     }
+
+    bool run_single_pass(PassIDType passid, bool force, bool post);
 };
 }; // namespace pass
