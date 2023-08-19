1984b467f780b552eb5eb13114c164eb74251b9d
lxq <877250099@qq.com>
2023-08-19 18:22:04 +0800

    feat(dce): parse stores that are not critical

--- src/transform/dead_code.cc
+++ src/transform/dead_code.cc
@@ -1,12 +1,15 @@
 #include "dead_code.hh"
+#include "err.hh"
 #include "func_info.hh"
 #include "function.hh"
 #include "global_variable.hh"
 #include "instruction.hh"
+#include "log.hh"
 #include "module.hh"
+#include "type.hh"
 #include "utils.hh"
 
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 using namespace pass;
@@ -17,6 +20,8 @@ bool DeadCode::run(PassManager *mgr) {
     auto m = mgr->get_module();
     changed = false;
     for (auto &f_r : m->functions()) {
+        if (f_r.is_external)
+            continue;
         auto f = &f_r;
         mark_sweep(f);
     }
@@ -27,6 +32,9 @@ bool DeadCode::run(PassManager *mgr) {
 void DeadCode::mark_sweep(Function *func) {
     work_list.clear();
     marked.clear();
+    store_not_critical.clear();
+    // prepare for is_critical
+    collect_store_not_critical(func);
     // Initialize the work list with critical instuction
     for (auto &bb : func->bbs()) {
         for (auto &inst_r : bb.insts()) {
@@ -76,8 +84,10 @@ void DeadCode::sweep(Function *func) {
 }
 
 bool DeadCode::is_critical(Instruction *inst) {
-    if (is_a<RetInst>(inst) || is_a<StoreInst>(inst) || is_a<BrInst>(inst))
+    if (is_a<RetInst>(inst) || is_a<BrInst>(inst))
         return true;
+    if (is_a<StoreInst>(inst))
+        return not contains(store_not_critical, inst);
     if (is_a<CallInst>(inst) &&
         not _func_info->is_pure_function(as_a<Function>(inst->operands()[0])))
         return true;
@@ -100,4 +110,46 @@ void DeadCode::sweep_globally(Module *m) {
         m->functions().erase(func);
     for (auto glob : unused_globals)
         m->global_vars().erase(glob);
+}
+
+void DeadCode::collect_store_not_critical(Function *func) {
+    for (auto &bb : func->bbs()) {
+        for (auto &alloca : bb.insts()) {
+            if (not is_a<AllocaInst>(&alloca))
+                continue;
+            if (alloca.get_type()
+                    ->as<PointerType>()
+                    ->get_elem_type()
+                    ->is_basic_type())
+                continue;
+            bool alloca_is_critical = false;
+            unordered_set<Instruction *> related_store;
+            for (auto &[arr_use, _] : alloca.get_use_list()) {
+                // store [value], arr-ptr
+                if (is_a<StoreInst>(arr_use)) {
+                    related_store.insert(as_a<Instruction>(arr_use));
+                    continue;
+                }
+                // gep arr-ptr, [offs]
+                for (auto &[use, _] :
+                     as_a<GetElementPtrInst>(arr_use)->get_use_list()) {
+                    if (is_a<StoreInst>(use)) {
+                        related_store.insert(as_a<Instruction>(use));
+                    } else {
+                        alloca_is_critical = true;
+                        break;
+                    }
+                }
+                if (alloca_is_critical)
+                    break;
+            }
+            if (not alloca_is_critical) {
+                auto old_set = store_not_critical;
+                set_union(
+                    related_store.begin(), related_store.end(), old_set.begin(),
+                    old_set.end(),
+                    inserter(store_not_critical, store_not_critical.begin()));
+            }
+        }
+    }
 }
